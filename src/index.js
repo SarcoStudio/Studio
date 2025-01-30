833415e37d7b2b70de7874eb52bd058d440fcfc1
SarcoStudio <kelvin.stokker@me.com>
2025-01-30 11:49:46 +0200

    Create app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css"; // You can create this later if needed
+import App from "./App"; // This will be your main app component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
